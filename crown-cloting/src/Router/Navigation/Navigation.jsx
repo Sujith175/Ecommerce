6f489c9f1421fa73f9f30ec19106ed87515e8f4c
Sujith175 <sujithkumar.sk175@gmail.com>
2022-12-08 13:47:23 +0530

    feat:navigation bar

---
+++ crown-cloting/src/Router/Navigation/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Navigation/navigation.styles.scss";
+import { Outlet, Link } from "react-router-dom";
+import crown from "../../assets/crown.svg";
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>
+            <img alt="" className="logo" src={crown} />
+          </div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
